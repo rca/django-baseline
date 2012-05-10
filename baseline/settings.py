db0b8a688f319f70688c946ccc02d8a113d68afd
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2012-05-10 10:48:50 -0700

    Updated warn() method.
    Added prefix and print_traceback kwargs.

--- baseline/settings.py
+++ baseline/settings.py
@@ -2,14 +2,17 @@ import sys
 
 from conf.settings.default import *
 
-def warn(message, color='yellow', name='Warning'):
-    import sys, traceback
+def warn(message, color='yellow', name='Warning', prefix='', print_traceback=True):
+    import sys
     from django.utils.termcolors import make_style
 
     red = make_style(fg=color, opts=('bold',))
 
-    traceback.print_exc()
-    sys.stderr.write('{0}: {1}\n'.format(red(name), message))
+    if print_traceback:
+        import traceback
+        traceback.print_exc()
+
+    sys.stderr.write('{0}{1}: {2}\n'.format(prefix, red(name), message))
 
 try:
     from secretkey import SECRET_KEY
