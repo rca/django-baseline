c747fd53506f6f3b7f02d4a9c0fcf4ead896b818
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2012-05-11 10:59:32 -0700

    Moved warn() to baseline.util

--- baseline/settings.py
+++ baseline/settings.py
@@ -1,18 +1,8 @@
 import os
 
+from baseline.util import warn
+
 from conf.settings.default import *
-
-def warn(message, color='yellow', name='Warning', prefix='', print_traceback=True):
-    import sys
-    from django.utils.termcolors import make_style
-
-    red = make_style(fg=color, opts=('bold',))
-
-    if print_traceback:
-        import traceback
-        traceback.print_exc()
-
-    sys.stderr.write('{0}{1}: {2}\n'.format(prefix, red(name), message))
 
 try:
     from localapps import LOCAL_APPS
---
+++ baseline/util.py
@@ -0,0 +1,11 @@
+def warn(message, color='yellow', name='Warning', prefix='', print_traceback=True):
+    import sys
+    from django.utils.termcolors import make_style
+
+    red = make_style(fg=color, opts=('bold',))
+
+    if print_traceback:
+        import traceback
+        traceback.print_exc()
+
+    sys.stderr.write('{0}{1}: {2}\n'.format(prefix, red(name), message))
