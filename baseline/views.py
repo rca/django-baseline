53427bfd06c709787473bc884d2cd9bfd994f62a
Roberto Aguilar <roberto@baremetal.io>
2013-03-05 00:48:49 -0800

    Updated views to be PEP-8 compliant.

--- baseline/views.py
+++ baseline/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+
 def explode(request):
     raise Exception('boom!')
 
+
 def home(request):
     context = {}
-    return render_to_response('baseline/home.html', context,
-                       context_instance=RequestContext(request))
+    return render_to_response(
+        'baseline/home.html', context, context_instance=RequestContext(request))
+
 
 def notfound(request):
     from django.http import Http404
     raise Http404
 
+
 def preview(request, template):
     context = {}
-    return render_to_response(template, context,
-                       context_instance=RequestContext(request))
+    return render_to_response(
+        template, context, context_instance=RequestContext(request))
