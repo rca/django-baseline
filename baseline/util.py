aa400f496966df4f9f1d72d2159fdc352bddb5a2
Roberto Aguilar <roberto@baremetal.io>
2013-03-28 16:01:11 -0700

    Cleaned up baseline util module.
    Added PEP-8 newlines and removed unused functions.

--- baseline/util.py
+++ baseline/util.py
@@ -10,6 +10,7 @@ HEROKU_CONFIG_RE = re.compile(r'(?P<key>[^:]*):\s+(?P<value>.*)')
 class HerokuError(Exception):
     pass
 
+
 def convert_bool(value):
     """
     Returns the value as a boolean if appropriate
@@ -24,6 +25,7 @@ def convert_bool(value):
 
     return value
 
+
 def convert_int(value):
     """
     Return the value as an integer if it looks like a number. The value will be
@@ -33,6 +35,7 @@ def convert_int(value):
         return int(value)
     except ValueError:
         return value
+
 
 def convert_sequence(value):
     if not isinstance(value, six.string_types):
@@ -67,42 +70,6 @@ def convert_sequence(value):
 
     return new_value
 
-def get_heroku_config():
-    heroku_config = {}
-    command = ['heroku', 'config']
-
-    try:
-        heroku = Popen(command, stdout=PIPE)
-    except OSError, exc:
-        if exc[0] != 2:
-            raise
-
-        raise HerokuError('Heroku config failed: {0}'.format(exc), ' '.join(command))
-
-    output = heroku.communicate()[0]
-    for line in output.splitlines():
-        matches = HEROKU_CONFIG_RE.match(line)
-        if matches:
-            heroku_config[matches.group('key')] = matches.group('value')
-
-    return heroku_config
-
-def set_heroku_config(**config):
-    args = ['{0}={1!r}'.format(a, b) for a, b in config.items()]
-    command = ['heroku', 'config:add'] + args
-
-    try:
-        heroku = Popen(command, stdout=PIPE, stderr=PIPE)
-    except OSError, exc:
-        if exc[0] != 2:
-            raise
-
-        raise HerokuError('Heroku config failed: {0}'.format(exc), ' '.join(command))
-
-    stdout, stderr = heroku.communicate()
-    status = heroku.wait()
-    if status != 0:
-        raise HerokuError('Heroku config failed: {0} (stdout: {1}, stderr: {2})'.format(' '.join(command), stdout, stderr), ' '.join(command))
 
 def warn(message, color='yellow', name='Warning', prefix='', print_traceback=True):
     import sys
