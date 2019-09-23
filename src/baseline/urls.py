0e49c9824308c9611cbcd1f125f10d49d3e44612
Roberto Aguilar <r@rreboto.com>
2019-09-23 07:43:33 -0400

    Refresh baseline django project

--- src/baseline/urls.py
+++ src/baseline/urls.py
@@ -1,39 +1,21 @@
-from django.conf.urls import patterns, include, url
-from django.conf import settings
-from django.utils.importlib import import_module
+"""baseline URL Configuration
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
 
-try:
-    from baseline.local_urls import urlpatterns
-except ImportError:
-    urlpatterns = []
-
-try:
-    from baseline.local_urls import fallback_urlpatterns
-except ImportError:
-    fallback_urlpatterns = []
-
-
-urlpatterns += patterns('',
-    # Examples:
-    url(r'^$', 'baseline.views.home', name='home'),
-    url(r'^404/$', 'baseline.views.notfound', name='404'),
-    url(r'^500/$', 'baseline.views.explode', name='500'),
-    url(r'^preview/404/$', 'baseline.views.preview', kwargs={'template': '404.html'}, name='404'),
-    url(r'^preview/500/$', 'baseline.views.preview', kwargs={'template': '500.html'}, name='500'),
-    # url(r'^baseline/', include('baseline.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    (r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-
-    url(r'^login/?$', 'django.contrib.auth.views.login', {'template_name': 'baseline/login.html'}, name='login'),
-    url(r'^logout/?$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'', include('social_auth.urls')),
-) + fallback_urlpatterns
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
