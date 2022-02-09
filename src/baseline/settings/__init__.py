dfe81ee93c4c7b4a5bc8bce750b933f1a49161d2
Roberto Aguilar <r@rreboto.com>
2022-02-09 10:04:44 -0500

    Wire rest framework into baseline settings

--- src/baseline/settings/__init__.py
+++ src/baseline/settings/__init__.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,15 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "baseline.permissions.FullDjangoModelPermissions",
+    ],
+}
