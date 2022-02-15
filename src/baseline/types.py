3da4126f904234032e60b4f326f76b146f6b448f
Roberto Aguilar <r@rreboto.com>
2022-02-15 10:37:57 -0500

    Add fixture to easily get django model instances

---
+++ src/baseline/types.py
@@ -0,0 +1,5 @@
+import typing
+
+from django.db.models.base import ModelBase
+
+ModelType = typing.Type[ModelBase]
--- src/conftest.py
+++ src/conftest.py
@@ -1,5 +1,55 @@
+import typing
+
+import pytest
+
+from django.contrib.auth import get_user_model
+
 from baseline import tests
+from baseline.types import ModelType
+
+if typing.TYPE_CHECKING:
+    from django.db.models import Model
+
+User = get_user_model()
+
+
+class ModelError(Exception):
+    """
+    Raised below
+    """
 
 
 def pytest_configure(config):
     tests.IS_PYTEST_RUNNING = True
+
+
+@pytest.fixture()
+def get_model(db):
+    """
+    Returns a function to get / create a instance
+    """
+
+    def wrapper(
+        model_cls: "ModelType", *args, create: bool = True, **kwargs
+    ) -> "Model":
+        """
+        Gets / Creates a Django model instance
+
+        Args:
+            model_cls: The type of object to retrieve
+            *args: any additional arguments to pass along to instance creation
+            create: when False a newly created instance will raise an exception.
+            **kwargs: any additional arguments to pass along to instance creation
+
+        Returns:
+
+        """
+        instance, created = model_cls.objects.get_or_create(*args, **kwargs)
+        if not create and created:
+            raise ModelError(
+                f"model_cls={model_cls}, args={args}, kwargs={kwargs} does not already exists"
+            )
+
+        return instance
+
+    return wrapper
