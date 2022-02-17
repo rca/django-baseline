d4e468dffcec5de636f95ddb199828d9d0e2a62d
Roberto Aguilar <r@rreboto.com>
2022-02-17 09:38:23 -0500

    Refactor out is_maintenance()

--- src/baseline/settings/utils.py
+++ src/baseline/settings/utils.py
@@ -3,3 +3,31 @@ import functools
 from ..environment import MaintenanceEnvironmentSetting, get_setting as base_get_setting
 
 get_setting = functools.partial(base_get_setting, MaintenanceEnvironmentSetting)
+
+
+def is_maintenance() -> bool:
+    """
+    Returns whether the current process is a test run
+    """
+    import sys
+
+    is_test = False
+    if sys.argv[0].endswith("manage.py") and (
+        len(sys.argv) == 1
+        or (
+            len(sys.argv) > 1
+            and sys.argv[1]
+            in (
+                "collectstatic",
+                "makemigrations",
+                "migrate",
+                "show_environment_settings",
+                "startapp",
+            )
+        )
+    ):
+        is_test = True
+    elif "pytest" in sys.argv[0]:
+        is_test = True
+
+    return is_test
