e49fc2916e0d833675a9e71405bdfcfc0c93eb30
Roberto Aguilar <r@rreboto.com>
2022-02-21 10:30:02 -0500

    Add DynamicFieldSerializer class
    This class will check for a `fields` param in a request and only return the requested fields.

--- src/dynamic_fields/tests.py
+++ src/dynamic_fields/tests.py
@@ -1,3 +1,111 @@
-from django.test import TestCase
+from unittest import mock
 
-# Create your tests here.
+from rest_framework import serializers
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+
+from bltestapp.models import Widget
+from bltestapp.serializers import WidgetSerializer
+from dynamic_fields.serializers import DynamicFieldSerializer
+
+
+class TestSerializer(DynamicFieldSerializer, serializers.Serializer):
+    first = serializers.CharField()
+    last = serializers.CharField()
+
+
+class TestExplicitPrefixSerializer(DynamicFieldSerializer, serializers.Serializer):
+    first = serializers.CharField()
+    last = serializers.CharField()
+
+    class Meta:
+        field_prefix = "ExplicitName"
+
+
+def test_serializer_prefix():
+    """
+    ensure the default prefix is created from the class name
+    """
+    serializer = TestSerializer()
+    assert "test" == serializer.field_prefix
+
+
+def test_explicit_serializer_prefix():
+    """
+    ensure the prefix is grabbed from the meta block
+    """
+    serializer = TestExplicitPrefixSerializer()
+    assert "explicitname" == serializer.field_prefix
+
+
+def test_existing_field_in_request_param():
+    """
+    ensure a field in the fields param that's already in the fields list does nothing
+    """
+    request_mock = mock.Mock(query_params=dict(fields="test_last"))
+
+    serializer = TestSerializer(
+        instance={"first": "test_first", "last": "test_last"},
+        context={"request": request_mock},
+    )
+
+    data = serializer.data
+
+    assert list(data.keys()) == ["first", "last"]
+
+
+def test_no_request_param():
+    """
+    ensure fields specified in the request are honored
+    """
+    request_mock = mock.Mock(query_params=dict())
+
+    serializer = TestSerializer(
+        instance={"first": "test_first", "last": "test_last"},
+        context={"request": request_mock},
+    )
+
+    data = serializer.data
+
+    assert list(data.keys()) == ["first", "last"]
+
+
+def test_dynamic_model_serializer_prefix():
+    """
+    Ensure the name comes from the model
+    """
+    serializer = WidgetSerializer()
+    assert "widget" == serializer.field_prefix
+
+
+def test_dynamic_model_fields():
+    """
+    ensure fields kwarg filters fields
+    """
+    widget = Widget(name="test widget", quantity=11)
+    serializer = WidgetSerializer(widget, fields="widget_id,widget_name")
+
+    assert sorted(list(serializer.data.keys())), serializer.data == ["id", "name"]
+
+
+def test_default_model_fields():
+    """
+    ensure fields kwarg filters fields
+    """
+    widget = Widget(name="test widget", quantity=11)
+    serializer = WidgetSerializer(widget)
+
+    assert sorted(list(serializer.data.keys())) == ["id", "name"]
+
+
+def test_api_request(db):
+    widget = Widget.objects.create(name="test widget", quantity=11)
+
+    client = APIClient()
+
+    url = reverse("widgets-list")
+    response = client.get(url, data={"fields": "widget_created,widget_name"})
+
+    assert 200 == response.status_code
+
+    assert sorted(list(response.data[0].keys())) == ["created", "id", "name"]
