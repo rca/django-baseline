ca28a01fda3c591c44f8905d5a5a726e877ced4a
Roberto Aguilar <r@rreboto.com>
2022-02-21 10:28:16 -0500

    Add a Widget model

--- src/bltestapp/models.py
+++ src/bltestapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from model_utils.models import TimeStampedModel
+
+from baseline.models.named_model import NamedModel
+from baseline.models.uuid_model import UUIDModel
+
+
+class Widget(UUIDModel, NamedModel, TimeStampedModel, models.Model):
+    """
+    A widget
+    """
+
+    quantity = models.PositiveIntegerField()
