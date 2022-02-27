84a1dd5dfd7fe94a2a5a5eb42bab0081b8c85dff
Roberto Aguilar <r@rreboto.com>
2022-02-27 12:33:48 -0500

    Add helper to return an operation for content types
    This operation flushes the content types into the database so that they are available for subsequent migration operations.

--- src/baseline/migrations/__init__.py
+++ src/baseline/migrations/__init__.py
@@ -1,6 +1,15 @@
+import functools
+import typing
+
 from django.apps.registry import apps as global_apps
 from django.contrib.auth.management import create_permissions
 from django.contrib.contenttypes.management import create_contenttypes
+from django.db import migrations
+
+if typing.TYPE_CHECKING:
+    from django.db.migrations.operations.base import Operation
+
+
 
 
 def create_content_types_and_permissions(app_name: str, apps, schema_editor):
@@ -28,3 +37,20 @@ def remove_content_types(app_name: str, apps, schema_editor):
     Stub for removing content types; this is currently a no-op
     """
     pass
+
+
+def setup_content_types(app_name: str) -> "Operation":
+    """
+    Sets content types and permissions in the database
+
+    Before running any migration operation that relies on permissions being
+    in the database, make sure all the app content types and permissions
+    are created.
+
+    Args:
+        app_name: the name of the django app to create permissions for
+    """
+    return migrations.RunPython(
+        functools.partial(create_content_types_and_permissions, app_name),
+        functools.partial(remove_content_types, app_name),
+    )
