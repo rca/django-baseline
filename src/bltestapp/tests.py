d764ff989171d65917a03b7cdfca323ecda5e15a
Roberto Aguilar <r@rreboto.com>
2022-02-27 23:28:46 -0500

    Add filtering settings and some tests around filter and search

--- src/bltestapp/tests.py
+++ src/bltestapp/tests.py
@@ -1,3 +1,63 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
 
-# Create your tests here.
+from bltestapp.fixtures import *
+
+
+def test_widget_filtering(get_api_client, get_widget):
+    """
+    Ensure filtering configuration works
+    """
+    for i in range(3):
+        get_widget(name=f"thing {i}")
+
+    client = get_api_client()
+
+    url = reverse("widgets-list")
+
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+
+    results = response.data["results"]
+
+    assert len(results) == 3
+
+    # filter by a full name
+    filter_name = "thing 1"
+    response = client.get(url, data=dict(name=filter_name))
+
+    results = response.data["results"]
+
+    assert len(results) == 1
+
+    assert results[0]["name"] == filter_name
+
+    # filter by a partial name
+    filter_name = " 2"
+    response = client.get(url, data=dict(name=filter_name))
+
+    results = response.data["results"]
+
+    assert len(results) == 1
+
+    assert results[0]["name"] == "thing 2"
+
+
+def test_widget_search(get_api_client, get_widget):
+    """
+    Ensure search works
+    """
+    for i in range(3):
+        get_widget(name=f"thing {i}")
+
+    client = get_api_client()
+
+    url = reverse("widgets-list")
+
+    filter_name = " 2"
+    response = client.get(url, data=dict(search=filter_name))
+
+    results = response.data["results"]
+
+    assert len(results) == 1, results
