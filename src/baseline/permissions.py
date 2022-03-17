c6ee0de4bcad3d48c6873ab77fa6e5fb86eb6b9f
Roberto Aguilar <r@rreboto.com>
2022-03-17 16:50:48 -0400

    Update FullDjangoModelPermissions to look for action permissions
    Update the permissions class to look for custom permissions that are tied to custom actions.  This uses a simple name based convention where the custom permission should have the same name as the custom endpoint.

--- src/baseline/permissions.py
+++ src/baseline/permissions.py
@@ -8,3 +8,19 @@ class FullDjangoModelPermissions(permissions.DjangoModelPermissions):
 
     perms_map = permissions.DjangoModelPermissions.perms_map.copy()
     perms_map.update({"GET": ["%(app_label)s.view_%(model_name)s"]})
+
+    def has_permission(self, request, view):
+        model_cls = view.queryset.model
+        kwargs = {
+            "action": view.action,
+            "app_label": model_cls._meta.app_label,
+            "model_name": model_cls._meta.model_name,
+        }
+
+        perms = ["{app_label}.{action}_{model_name}".format(**kwargs)]
+
+        has_perms = request.user.has_perms(perms)
+        if not has_perms:
+            return super().has_permission(request, view)
+
+        return has_perms
