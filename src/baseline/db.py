2a6ca5335121c61ec402884cdd14b1a94dac16ad
Roberto Aguilar <r@rreboto.com>
2022-03-29 16:44:52 -0400

    Make get_for_update() non-blocking

--- src/baseline/db.py
+++ src/baseline/db.py
@@ -1,14 +1,17 @@
-def get_for_update(model_item):
+def get_for_update(model_item, nowait: bool = True):
     """
     Returns the same instance of the model item retrieved from the database with `.select_for_update()`
 
+    By default this is a non-blocking call; to change the behavior, set nowait=False
+
     Args:
         model_item: the model item to re-fetch from the database
+        nowait: whether to block until the row is unlocked
 
     Returns:
         the model item
     """
-    queryset = model_item._meta.model.objects.select_for_update()
+    queryset = model_item._meta.model.objects.select_for_update(nowait=nowait)
     model_item_for_update = queryset.get(pk=model_item.pk)
 
     return model_item_for_update
