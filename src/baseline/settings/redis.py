7300a2ecb6334de948ad2d76472564943876c08b
Roberto Aguilar <r@rreboto.com>
2022-04-15 16:35:30 -0400

    Add docker-accessible redis cache url as the maintenance default

--- src/baseline/settings/redis.py
+++ src/baseline/settings/redis.py
@@ -3,7 +3,7 @@ Settings module to configure a basic Redis cache
 """
 from .utils import get_setting
 
-REDIS_CACHE_URL = get_setting("REDIS_CACHE_URL", maintenance_default="")
+REDIS_CACHE_URL = get_setting("REDIS_CACHE_URL", maintenance_default="redis://redis:6379/0")
 
 CACHES = {
     "default": {
