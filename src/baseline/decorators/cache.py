9efad1a82e3b1fedb450638fd236a8c8fdd2ca03
Roberto Aguilar <r@rreboto.com>
2022-12-17 20:09:30 -0500

    Add combined_cache_control decorator
    Adds all the necessary bits to a response for effective caching

--- src/baseline/decorators/cache.py
+++ src/baseline/decorators/cache.py
@@ -1,6 +1,10 @@
 import functools
 
 from django.core.cache import cache
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_control
+from django.views.decorators.http import condition
+from django.views.decorators.vary import vary_on_headers
 
 
 def cached(name: str = None, timeout=None):
@@ -31,5 +35,33 @@ def cached(name: str = None, timeout=None):
             return result
 
         return wrapper
+
+    return decorator
+
+
+def combined_cache_control(
+    max_age, etag_func, last_modified_func, cache_control_kwargs=None, vary_headers=None
+):
+    _max_age = max_age
+    _cache_control_kwargs = cache_control_kwargs or dict(must_revalidate=True)
+    _vary_headers = vary_headers or ("Authorization",)
+    _etag_func = etag_func
+    _last_modified_func = last_modified_func
+
+    """
+    A single decorator to apply all the headers needed for high-performance page caching
+    """
+
+    def decorator(fn):
+        return method_decorator(vary_on_headers(*_vary_headers))(
+            method_decorator(cache_control(max_age=_max_age, **_cache_control_kwargs))(
+                method_decorator(
+                    condition(
+                        etag_func=_etag_func,
+                        last_modified_func=_last_modified_func,
+                    )
+                )(fn)
+            )
+        )
 
     return decorator
