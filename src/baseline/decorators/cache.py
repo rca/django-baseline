ebcec864d3c53f785de1e7cebfc2da78b0166c76
Roberto Aguilar <r@rreboto.com>
2022-12-17 20:09:07 -0500

    Add timeout to cached decorator

--- src/baseline/decorators/cache.py
+++ src/baseline/decorators/cache.py
@@ -3,12 +3,14 @@ import functools
 from django.core.cache import cache
 
 
-def cached(name: str = None):
+def cached(name: str = None, timeout=None):
     """
     Cache decorator
 
     Args:
         name: the name to use, otherwise the function name is used
+        timeout: the amount of time (in seconds) to persist the data in
+        the cache
     """
 
     def decorator(fn):
@@ -21,7 +23,10 @@ def cached(name: str = None):
                 return result
 
             result = fn(self, *args, **kwargs)
-            cache.set(cache_key, result, timeout=86400 * 7)
+            timeout_arg = {}
+            if timeout:
+                timeout_arg["timeout"] = timeout
+            cache.set(cache_key, result, **timeout_arg)
 
             return result
 
