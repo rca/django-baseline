60bff2315f592266601b6029464c6fd730f81c9d
Roberto Aguilar <r@rreboto.com>
2023-02-28 11:33:13 -0500

    Updates to baseline, cache app, and webproject

--- src/baseline/settings/__init__.py
+++ src/baseline/settings/__init__.py
@@ -76,6 +76,7 @@ if is_test():
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -153,6 +154,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+print(f"STATIC_ROOT={STATIC_ROOT}")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
