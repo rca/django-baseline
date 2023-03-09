119491b453a0b511e98f63470117212779fdd196
Roberto Aguilar <r@rreboto.com>
2023-03-09 10:54:00 -0500

    Add AccountViewSet

---
+++ src/baseline/views/account.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from baseline.serializers.user_serializer import UserSerializer
+
+User = get_user_model()
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint for getting logged in account details
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        print(self.kwargs)
+        pk = self.kwargs.get("pk")
+        if pk == "self":
+            pk = self.request.user.pk
+            self.kwargs.update(dict(pk=pk))
+
+        return super().get_object()
