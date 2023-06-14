91f3403608fa3cce857849b95d999f04be0a147a
Roberto Aguilar <r@rreboto.com>
2023-06-14 16:05:13 -0400

    Add debug for environment

--- src/baseline/settings/environment.py
+++ src/baseline/settings/environment.py
@@ -1,4 +1,8 @@
+import sys
+
 from .utils import get_setting
 
-ENVIRONMENT = get_setting("ENVIRONMENT", default="unknown")
+ENVIRONMENT = get_setting("CF_ENV", default="unknown")
 VERSION = get_setting("VERSION", required=False)
+
+print(f"ENVIRONMENT={ENVIRONMENT}, VERSION={VERSION}", file=sys.stderr)
