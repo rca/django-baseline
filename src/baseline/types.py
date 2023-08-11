22659652312217356f5011c7533f8d1ba43e1368
Roberto Aguilar <r@rreboto.com>
2023-08-10 23:47:58 -0400

    Add OptionalDate type

--- src/baseline/types.py
+++ src/baseline/types.py
@@ -2,6 +2,10 @@ import typing
 
 from django.db.models.base import ModelBase
 
+if typing.TYPE_CHECKING:
+    from datetime import date
+
 ModelType = typing.Type[ModelBase]
+OptionalDate = typing.Optional["date"]
 StringList = typing.Iterable[str]
 UUIDString = str
