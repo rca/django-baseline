6986bec6011f5d8991927542d64003dd8caa7c9d
Roberto Aguilar <r@rreboto.com>
2023-10-21 20:51:04 -0400

    Add two factor to installed apps for tests to run

--- src/bltestproject/settings.py
+++ src/bltestproject/settings.py
@@ -17,3 +17,15 @@ WSGI_APPLICATION = "bltestproject.wsgi.application"
 
 if BASELINE_TEST_APP not in INSTALLED_APPS:
     INSTALLED_APPS += [BASELINE_TEST_APP]
+
+# for tests to run
+INSTALLED_APPS += [
+    "django_otp",
+    "django_otp.plugins.otp_static",
+    "django_otp.plugins.otp_totp",
+    "django_otp.plugins.otp_email",  # <- if you want email capability.
+    "two_factor",
+    # "two_factor.plugins.phonenumber",  # <- if you want phone number capability.
+    "two_factor.plugins.email",  # <- if you want email capability.
+    # 'two_factor.plugins.yubikey',  # <- for yubikey capability.
+]
